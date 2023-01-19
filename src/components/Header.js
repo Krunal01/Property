b9dc677843fa4d91a04d3d96f2b72c6ba1ae9b0b
Krunal01 <parmarkrunal09101@gmail.com>
2023-01-19 15:29:43 +0530

    navbar field add as property add

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -24,6 +25,14 @@ function Header() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link>
+              <Link
+                to="property-add"
+                style={{ textDecoration: "none", color: "#0000008C" }}
+              >
+                Property Add
+              </Link>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
